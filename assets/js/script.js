667f7e575d602631f746fc2785c1dcafd9d4403d
Mutasim Billah <mutasim.wre@gmail.com>
2016-12-04 23:15:05 +0600

    header animation added, position fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,37 @@
+var addClass = function(elem, strClass) {
+  elem.classList.add(strClass);
+}
+
+var removeClass = function(elem, strClass) {
+  elem.classList.remove(strClass);
+}
+
 window.addEventListener("load", function() {
 
   var prelSec = document.getElementById("preloader");
-  prelSec.classList.remove("visible");
+  var socialBtns = document.querySelectorAll("header .social .fa");
+
+  removeClass(prelSec, "visible");
 
   setTimeout(function() {
-    
+
+    addClass(document.querySelector("header h1.animate"), "fadeInRight");
+    addClass(document.querySelector("header h4.animate"), "fadeInRight");
+
+  }, 250);
+
+  setTimeout(function() {
+
     prelSec.parentNode.removeChild(prelSec);
 
   }, 500);
+
+  setTimeout(function() {
+
+    for(i = 0; i < socialBtns.length; i++) {
+      addClass(socialBtns[i], "fadeInUp");
+    }
+
+  }, 750);
 
 });
